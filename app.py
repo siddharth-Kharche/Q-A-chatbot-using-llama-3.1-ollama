8b6a3d84eabb35d3efc6e083a7c1211f624aa8de
siddharth kharche <156710270+siddharth-Kharche@users.noreply.github.com>
2024-08-03 22:30:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import openai
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
